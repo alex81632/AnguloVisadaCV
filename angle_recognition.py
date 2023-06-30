d6e6ff5b832e75f12209fdcc78b7afc6f89b6394
alex81632 <alexandrebellargus@hotmail.com>
2023-06-29 22:07:56 -0300

    with model

--- angle_recognition.py
+++ angle_recognition.py
@@ -3,6 +3,10 @@ import dlib
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import pickle
+model = pickle.load(open('model.pkl', 'rb'))
 
 # Inicializar o detector de faces
 detector = dlib.get_frontal_face_detector()
@@ -68,13 +72,17 @@ def calcular_angulo_visada(pontos):
     dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
     (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, matriz_camera, dist_coeffs)
     
-    print("Rotation Vector:\n {0}".format(rotation_vector))
-    print("Translation Vector:\n {0}".format(translation_vector))
+    # printar o vetor de rotacao sem quebra de linha
+    # print("Vetor de Rotacao: {0}, {1}, {2}".format(rotation_vector[0], rotation_vector[1], rotation_vector[2]))
 
     (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, matriz_camera, dist_coeffs)
 
     return image_points, nose_end_point2D, rotation_vector, translation_vector    
 
+dados_rotacao = []
+
+in_atention = []
+out_of_atention = []
 
 while True:
     # Ler o frame da câmera
@@ -96,7 +104,31 @@ while True:
             y = shape.part(i).y
             cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
 
-        image_points, nose_end_point2D, rotation_vector, translation_vector = calcular_angulo_visada(shape) 
+        image_points, nose_end_point2D, rotation_vector, translation_vector = calcular_angulo_visada(shape)
+
+        dados_rotacao.append(rotation_vector)
+
+        x_adjusted = rotation_vector[0]+math.pi
+
+        if x_adjusted > math.pi:
+            x_adjusted = x_adjusted - 2*math.pi
+
+        # salvar o x_adjusted no rotatio_vector
+        rotation_vector[0] = x_adjusted
+
+        atencao = 0
+        
+        columns = ['x', 'y', 'z']
+        x, y, z = rotation_vector[0][0], rotation_vector[1][0], rotation_vector[2][0]
+        df = pd.DataFrame([[x, y, z]], columns=columns)
+        atencao = model.predict(df)
+        
+        # se esta olhando para a camera, fazer retangulo verde ao redor da face
+        if atencao == 1:
+            cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0, 255, 0), 2)
+        else:
+            cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()), (0, 0, 255), 2)
+
 
         for p in image_points:
             cv2.circle(frame, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
@@ -107,13 +139,45 @@ while True:
 
         cv2.line(frame, p1, p2, (255,0,0), 2)
 
-    # Mostrar o frame resultante
-    cv2.imshow('Pontos Faciais', frame)
+    # Mostrar o frame resultante invertida horizontalmente
+    cv2.imshow('Pontos Faciais', cv2.flip(frame, 1))
 
     # Verificar se a tecla 'q' foi pressionada para sair do loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    if cv2.waitKey(1) & 0xFF == ord('a'):
+        in_atention.append(rotation_vector)
+        print("in atention")
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        out_of_atention.append(rotation_vector)
+        print("out of atention")
+
+# # salvar em um txt os dados de in_atention e out_of_atention juntos em um mesmo arquivo com a diferenciaçao de 0 e 1
+# colums = ['x', 'y', 'z', 'atencao']
+# df = pd.DataFrame(columns=colums)
+
+# for i in range(len(in_atention)):
+#     df = df.append({'x': in_atention[i][0], 'y': in_atention[i][1], 'z': in_atention[i][2], 'atencao': 1}, ignore_index=True)
+
+# for i in range(len(out_of_atention)):
+#     df = df.append({'x': out_of_atention[i][0], 'y': out_of_atention[i][1], 'z': out_of_atention[i][2], 'atencao': 0}, ignore_index=True)
+
+# df.to_csv('dados_rotacao.csv', index=False)
+
 
 # Liberar os recursos
 cap.release()
 cv2.destroyAllWindows()
+
+dados_rotacao = np.array(dados_rotacao)
+
+# se o angulo for maior que pi, subtrair 2pi
+for i in range(len(dados_rotacao[:,0])):
+    if dados_rotacao[i,0] > math.pi:
+        dados_rotacao[i,0] = dados_rotacao[i,0] - 2*math.pi
+
+plt.plot(dados_rotacao[:,0], label='x')
+plt.plot(dados_rotacao[:,1], label='y')
+plt.plot(dados_rotacao[:,2], label='z')
+plt.legend()
+plt.show()
